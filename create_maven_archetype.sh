580bf7f37d910170ece74ddb7046aa3caecca775
genxium <genxium@hotmail.com>
2022-07-24 15:39:37 +0800

    Minor fix.

--- create_maven_archetype.sh
+++ create_maven_archetype.sh
@@ -8,20 +8,22 @@ if [ $# -ne 2 ]; then
 fi
 
 basedir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
+groupIdSuffix=$1
 groupId=com.mytrial.$groupIdSuffix
 archetypeArtifactId=maven-archetype-quickstart
 archetypeVersion=1.4
 
-artifactId=$1-spawned
+artifactId=$2-spawned
 
 function create_project() {
   if [[ ! -d $basedir/$artifactId ]]; then
     echo "Creating project $artifactId"
-    # By default uses offline mode option "-o"
+    # By default uses offline mode option "-o", remove it if necessary, e.g. in a new environment
+    # Add the "-<archetypeCatalog>" for faster catalog search, see https://maven.apache.org/archetype/maven-archetype-plugin/generate-mojo.html#archetypeCatalog for more information.
     cd $basedir && mvn -X -o archetype:generate -DgroupId=$groupId -DartifactId=$artifactId -DarchetypeArtifactId=$archetypeArtifactId -DarchetypeVersion=$archetypeVersion -DinteractiveMode=false
   fi
   cp $basedir/pom-file-templates/artifact-pom.xml $basedir/$artifactId/pom.xml 
+  sed -i "s/TO_BE_REPLACED_GROUP_ID_SUFFIX/$groupIdSuffix/g" $basedir/$artifactId/pom.xml
   sed -i "s/TO_BE_REPLACED/$artifactId/g" $basedir/$artifactId/pom.xml
 }
 
